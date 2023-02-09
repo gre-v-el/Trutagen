b2d7691a20ea3c1d8bdba3a07486621e3e6d47f5
gre-v-el <myszkier.gabriel@gmail.com>
2023-02-09 06:52:42 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,320 @@
+// after input surround parentheses with spaces, so they always count as separate words
+
+const FULL_BOX: [[&str; 3]; 3] = [
+	["\u{250F}", "\u{2533}", "\u{2513}"],
+	["\u{2523}", "\u{254B}", "\u{252B}"],
+	["\u{2517}", "\u{253B}", "\u{251B}"],
+];
+const VERTICAL_LINE: &str = "\u{2503}";
+const HORIZONTAL_LINE: &str = "\u{2501}";
+
+const ERROR_MSG: &str = "
+Some helpful info:
+Accepted tokens are:
+ - Variable name (more on that later)
+ - Or
+ - Nor
+ - Xor
+ - And
+ - Nand
+ - Not
+ - (
+ - )
+
+Expressions are not case sensitive - you can use lower- and uppercase letters, it will make no difference
+Variable names should contain only letters and no spaces.
+There should be whitespaces between tokens (parentheses are an exception).
+";
+
+#[derive(Debug, PartialEq, Eq)]
+enum Gate {
+	Or,
+	Nor,
+	Xor,
+	And,
+	Nand,
+	Not,
+	OpenParenthesis,
+	CloseParenthesis,
+}
+
+impl Gate {
+	fn precedence(&self) -> u8{
+		match &self {
+			Gate::Or => 1,
+			Gate::Nor => 1,
+			Gate::Xor => 1,
+			Gate::And => 2,
+			Gate::Nand => 2,
+			Gate::Not => 3,
+			Gate::CloseParenthesis => 0,
+			Gate::OpenParenthesis => 0,
+		}
+	}
+}
+
+#[derive(Debug)]
+enum Token {
+	Var(usize), // it holds an id of a variable in /variables/ vector
+	Gate(Gate),
+}
+
+#[derive(Debug, Clone)]
+struct Variable {
+	name: String,
+	value: bool,
+}
+
+fn main() {
+
+    println!("Enter a logic expression");
+	let mut expr = get_input();
+	prepare_input(&mut expr);
+	
+
+	let mut variables: Vec<Variable> = Vec::new();
+	let mut tokens = string_to_tokens(&expr, &mut variables);
+	//println!("parsed tokens:    {:?}", tokens);
+
+	let postline = inline_to_postline(&mut tokens);
+
+	// println!("found variables:  {:?}", variables);
+	// println!("postline:         {:?}", postline);
+
+	let all_combinations = get_all_combinations(&mut variables);
+	match evaluate_all(&postline, &all_combinations) {
+		Option::Some(v) => {
+			println!("\n");
+			let width = print_table(&all_combinations, &v);
+			println!("{}{}\n\n", " ".repeat((((width as i16)-(expr.len() as i16)).max(0)/2) as usize), expr.to_uppercase());
+		}
+		Option::None => {
+			println!("\n\"{}\" is not a valid expression!", expr.to_uppercase());
+			println!("{}", ERROR_MSG);
+		}
+	}
+}
+
+fn print_table(combinations: &Vec<Vec<Variable>>, answers: &Vec<bool>) -> usize {
+
+	let mut max_length = 1;
+	for v in &combinations[0] {
+		max_length = max_length.max(v.name.len());
+	}
+	max_length = max_length/2*2 + 1;
+	let width: usize;
+	if max_length > 1 { width = max_length + 2; }
+	else { width = max_length + 4; }
+	let padding: usize = (width-1)/2;
+
+
+	// upper edge
+	print!("{}", FULL_BOX[0][0]); 
+	for _ in 1..combinations[0].len() {
+		print!("{}{}", HORIZONTAL_LINE.repeat(width), FULL_BOX[0][1]);
+	}
+	println!("{}{}{}{}", HORIZONTAL_LINE.repeat(width), FULL_BOX[0][1], HORIZONTAL_LINE.repeat(9), FULL_BOX[0][2]);
+
+	// the line with variable names
+	print!("{}", VERTICAL_LINE);
+	for i in 0..combinations[0].len() {
+		print!("{0}{3}{1}{2}", 
+			" ".repeat((width-combinations[0][i].name.len())/2), 
+			" ".repeat(width - (width-combinations[0][i].name.len())/2 - combinations[0][i].name.len()), 
+			VERTICAL_LINE, 
+			combinations[0][i].name);
+	}
+	println!(" result  {}", VERTICAL_LINE);
+
+	// divider
+	print!("{}", FULL_BOX[1][0]); 
+	for _ in 1..combinations[0].len() {
+		print!("{}{}", HORIZONTAL_LINE.repeat(width), FULL_BOX[1][1]);
+	}
+	println!("{}{}{}{}", HORIZONTAL_LINE.repeat(width), FULL_BOX[1][1], HORIZONTAL_LINE.repeat(9), FULL_BOX[1][2]);
+	// print options
+	for i in 0..answers.len() {
+		for j in 0..combinations[i].len() {
+			print!("{1}{0}{2}{0}", " ".repeat(padding), VERTICAL_LINE, combinations[i][j].value as u8);
+		}
+		println!("{}    {}    {}", VERTICAL_LINE, answers[i] as u8, VERTICAL_LINE);
+	}
+
+	
+	// lower edge
+	print!("{}", FULL_BOX[2][0]); 
+	for _ in 1..combinations[0].len() {
+		print!("{}{}", HORIZONTAL_LINE.repeat(width), FULL_BOX[2][1]);
+	}
+	println!("{}{}{}{}", HORIZONTAL_LINE.repeat(width), FULL_BOX[2][1], HORIZONTAL_LINE.repeat(9), FULL_BOX[2][2]);
+
+	12 + width + (combinations[0].len() - 1)*(width + 1) // table width
+}
+
+fn evaluate_all(t: &Vec<Token>, v: &Vec<Vec<Variable>>) -> Option<Vec<bool>> {
+	let mut out: Vec<bool> = Vec::with_capacity(v.len());
+
+	for i in 0..v.len() {
+		match evaluate(&t, &v[i]) {
+			Option::Some(b) => out.push(b),
+			Option::None => return Option::None,
+		}
+	}
+
+	Option::Some(out)
+}
+
+fn get_all_combinations(variables: &mut Vec<Variable>) -> Vec<Vec<Variable>> {
+	let mut out: Vec<Vec<Variable>> = Vec::with_capacity((2 as usize).pow(variables.len() as u32));
+
+	for i in 0..out.capacity() {
+		out.push(Vec::with_capacity(variables.len()));
+		for j in 0..variables.len() {
+			let mut clone = variables[j].clone();
+			clone.value = i>>(variables.len() - 1 - j) & 1 > 0;
+			out[i].push(clone);
+		}
+	}
+
+
+	out
+}
+
+fn evaluate(v: &Vec<Token>, variables: &Vec<Variable>) -> Option<bool> {
+	let mut stack: Vec<bool> = Vec::new();
+	
+	for t in v {
+		match t {
+			Token::Var(index) => stack.push(variables.get(*index).unwrap().value),
+			Token::Gate(gate) => {
+
+				if let Gate::Not = gate {
+					if stack.len() < 1 { return Option::None; }
+					
+					let one = stack.pop().unwrap();
+					stack.push(!one);
+				}
+				else {
+					if stack.len() < 2 { return Option::None; }
+
+					let one = stack.pop().unwrap();
+					let two = stack.pop().unwrap();
+
+					match gate {
+						Gate::Or => stack.push(one || two),
+						Gate::Nor => stack.push(!(one || two)),
+						Gate::Xor => stack.push(one ^ two),
+						Gate::And => stack.push(one && two),
+						Gate::Nand => stack.push(!(one && two)),
+						_ => {},
+					}
+				}
+
+				
+			}
+		}
+	}
+
+	if stack.len() == 1 { return Option::Some(stack.pop().unwrap()); }
+	Option::None
+}
+
+fn inline_to_postline(v: &mut Vec<Token>) -> Vec<Token> { // shunting yard algorithm
+	let mut out: Vec<Token> = Vec::new();
+
+	let mut operator_stack: Vec<Gate> = Vec::new();
+	
+	while v.len() > 0 {
+		match v.remove(0) {
+			Token::Var(i) => {
+				out.push(Token::Var(i));
+			}
+			Token::Gate(g) => {
+
+				match g {
+					Gate::OpenParenthesis => operator_stack.push(Gate::OpenParenthesis),
+					Gate::CloseParenthesis => {
+						while *operator_stack.last().unwrap() != Gate::OpenParenthesis {
+							out.push(Token::Gate(operator_stack.pop().unwrap()));
+						}
+						operator_stack.pop();
+					},
+					gate => {
+						while operator_stack.len() > 0 && 
+							  operator_stack.last().unwrap().precedence() >= gate.precedence() 
+						{
+								out.push(Token::Gate(operator_stack.pop().unwrap()));
+						}
+						operator_stack.push(gate);
+					}
+				}
+			}
+		}
+	}
+
+	while operator_stack.len() > 0 {
+		if *operator_stack.last().unwrap() == Gate::CloseParenthesis || *operator_stack.last().unwrap() == Gate::OpenParenthesis  {panic!()}
+		out.push(Token::Gate(operator_stack.pop().unwrap()));
+	}
+	
+
+	out
+}
+
+fn string_to_tokens(s: &String, variables: &mut Vec<Variable>) -> Vec<Token> {
+	let mut v: Vec<Token> = Vec::new();
+
+	for w in s.split_whitespace() {
+		v.push(word_to_token(w, variables));
+	}
+
+	v
+}
+
+fn word_to_token(w: &str, variables: & mut Vec<Variable>) -> Token {
+	match w.to_uppercase().as_str() {
+		"OR" => Token::Gate(Gate::Or),
+		"NOR" => Token::Gate(Gate::Nor),
+		"XOR" => Token::Gate(Gate::Xor),
+		"AND" => Token::Gate(Gate::And),
+		"NAND" => Token::Gate(Gate::Nand),
+		"NOT" => Token::Gate(Gate::Not),
+		"(" => Token::Gate(Gate::OpenParenthesis),
+		")" => Token::Gate(Gate::CloseParenthesis),
+		b => {
+			match variables.iter().position(|e| e.name == b) {
+				Some(index) => {
+					Token::Var(index)
+				},
+				None => {
+					variables.push(Variable{ name: b.to_string(), value: false });
+					Token::Var(variables.len()-1)
+				}
+			}
+		},
+	}
+}
+
+fn prepare_input(s: &mut String) {
+	let mut i = 0;
+
+	while i < s.len() {
+		if s.chars().nth(i).unwrap() == '(' || s.chars().nth(i).unwrap() == ')' {
+			if i != s.len()-1 && s.chars().nth(i+1).unwrap() != ' ' { s.insert(i+1, ' '); }
+			if i != 0 && s.chars().nth(i-1).unwrap() != ' ' { 
+				s.insert(i,   ' '); 
+				i+=1;
+			}
+		}
+		i+=1;
+	}
+}
+
+fn get_input() -> String {
+	use std::io::stdin;
+
+	let mut ret = String::new();
+	stdin().read_line(&mut ret).expect("Failed to read a line");
+
+	ret.trim().to_string()
+}
